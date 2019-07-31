78dba062a08f7862dce89e21755e207d59dc1ec8
ableigdev <igor.yarovoy.98@ukr.net>
2019-07-31 09:26:26 +0300

    Add unit tests for the ConsoleViewerImpl class

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,8 +1,14 @@
 #include <memory>
+#include <vector>
 #include "gtest/gtest.h"
 
 #include "IFactory.h"
 #include "ConsoleFactoryImpl.h"
+#include "IViewer.h"
+#include "ConsoleViewerImpl.h"
+#include "ConsoleSnakeImpl.h"
+#include "ConsoleFruitImpl.h"
+#include "SecondConsoleFruitImpl.h"
 
 TEST(FactoryTest, Test1)
 {
@@ -39,6 +45,64 @@ TEST(FactoryTest, Test3)
     auto viewer(factory->createViewer(std::move(snake), std::move(fruits)));
 
     EXPECT_TRUE(viewer != nullptr);
+}
+
+TEST(ConsoleViewerImpl, SnakeBodySize)
+{
+    auto snake(std::make_unique<snake_impl::console_snake_impl::ConsoleSnakeImpl>());
+    auto fruit1(std::make_unique<fruit_impl::console_fruit_impl::ConsoleFruitImpl>());
+    auto fruit2(std::make_unique<fruit_impl::console_fruit_impl::SecondConsoleFruitImpl>());
+
+    std::vector<std::unique_ptr<interfaces::IFruit>> fruits;
+    fruits.push_back(std::move(fruit1));
+    fruits.push_back(std::move(fruit2));
+
+    auto consoleViewer(std::make_unique<viewer_impl::console_viewer_impl::ConsoleViewerImpl>(std::move(snake), std::move(fruits)));
+    consoleViewer->resetSnake();
+
+    EXPECT_EQ(consoleViewer->getAllSnakeBody().size(), 3);
+
+    consoleViewer->addPointOfBody({ 5, 5 });
+    EXPECT_EQ(consoleViewer->getAllSnakeBody().size(), 4);
+
+    consoleViewer->addPointOfBodyToBegin({ 6, 7 });
+    EXPECT_EQ(consoleViewer->getAllSnakeBody().size(), 5);
+}
+
+TEST(ConsoleViewerImpl, SnakeBodySize2)
+{
+    auto snake(std::make_unique<snake_impl::console_snake_impl::ConsoleSnakeImpl>());
+    auto fruit1(std::make_unique<fruit_impl::console_fruit_impl::ConsoleFruitImpl>());
+    auto fruit2(std::make_unique<fruit_impl::console_fruit_impl::SecondConsoleFruitImpl>());
+
+    std::vector<std::unique_ptr<interfaces::IFruit>> fruits;
+    fruits.push_back(std::move(fruit1));
+    fruits.push_back(std::move(fruit2));
+
+    auto consoleViewer(std::make_unique<viewer_impl::console_viewer_impl::ConsoleViewerImpl>(std::move(snake), std::move(fruits)));
+    consoleViewer->resetSnake();
+
+    EXPECT_EQ(consoleViewer->getAllSnakeBody().size(), 3);
+
+    consoleViewer->removeOldTail();
+    EXPECT_EQ(consoleViewer->getAllSnakeBody().size(), 2);
+}
+
+TEST(ConsoleViewerImpl, Score)
+{
+    auto snake(std::make_unique<snake_impl::console_snake_impl::ConsoleSnakeImpl>());
+    auto fruit1(std::make_unique<fruit_impl::console_fruit_impl::ConsoleFruitImpl>());
+    auto fruit2(std::make_unique<fruit_impl::console_fruit_impl::SecondConsoleFruitImpl>());
+
+    std::vector<std::unique_ptr<interfaces::IFruit>> fruits;
+    fruits.push_back(std::move(fruit1));
+    fruits.push_back(std::move(fruit2));
+
+    auto consoleViewer(std::make_unique<viewer_impl::console_viewer_impl::ConsoleViewerImpl>(std::move(snake), std::move(fruits)));
+    
+    EXPECT_EQ(consoleViewer->score(), 0);
+    consoleViewer->updateScore(15);
+    EXPECT_EQ(consoleViewer->score(), 15);
 }
 
 int main(int argc, char** argv)
